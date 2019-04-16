ecd548edf8e8cc23c7e3adad2d492b91cf7dd879
marcuspereiragithub <vinnycoast@gmail.com>
2019-04-16 15:21:17 -0300

    Trying to upload a file

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,6 @@ const BoxController = require('./controllers/BoxController');
 const FileController = require('./controllers/FileController');
 
 routes.post('/boxes', BoxController.store);
-routes.post('/files', multer(multerConfig).single('file'), FileController.store)
+routes.post('/boxes/:id/files', multer(multerConfig).single('file'), FileController.store)
 
 module.exports = routes;
